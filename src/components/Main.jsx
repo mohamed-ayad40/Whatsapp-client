01c97c376fa60124ad363e6519a3d9477f90754e
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-09-21 02:35:14 +0300

    m

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -52,7 +52,7 @@ function Main() {
       console.log(HOST);
       socket.current = io(HOST, {
         path: '/socket.io',         // Match the backend path
-        reconnection: false,        // Keep reconnection disabled for now
+        reconnection: true,        // Keep reconnection disabled for now
         timeout: 5000,              // Timeout after 5 seconds if it can't connect
         transports: ["websocket"],  // Force WebSocket transport only (no polling)
         withCredentials: true,      // Ensure cookies are sent with requests (if needed for auth)
