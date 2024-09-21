7cac59f1a912e48f816326bd1524fb0d49a20cb9
mohamed-ayad40 <mohamedayad5600@gmail.com>
2024-09-21 03:15:07 +0300

    m

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -88,13 +88,13 @@ function Main() {
 
       socket.current.on("connect_error", (err) => {
         // the reason of the error, for example "xhr poll error"
-        console.log(err.message);
+        console.log("message is", err.message);
       
         // some additional description, for example the status code of the initial HTTP response
-        console.log(err.description);
+        console.log("desc is", err.description);
       
         // some additional context, for example the XMLHttpRequest object
-        console.log(err.context);
+        console.log("context is", err.context);
       });
       
       socket.current.on("incoming-voice-call", ({from, roomId, callType}) => {
