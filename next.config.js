f7626835f32cf8008af899036ca67756122af0f9
mohamed-ayad40 <mohamed-ayad5600>
2026-05-04 22:36:34 +0300

    Update Next config to use remotePatterns for Cloudinary

--- next.config.js
+++ next.config.js
@@ -2,12 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_ZEGO_APP_ID: Number.parseInt(process.env.NEXT_PUBLIC_ZEGO_APP_ID),
+    NEXT_PUBLIC_ZEGO_APP_ID: process.env.NEXT_PUBLIC_ZEGO_APP_ID,
     NEXT_PUBLIC_ZEGO_SERVER_ID: process.env.NEXT_PUBLIC_ZEGO_SERVER_ID
   },
   images: {
-    domains: ["localhost", "res.cloudinary.com"],
-    
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
